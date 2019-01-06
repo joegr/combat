1ef49ae1e5fb4c7b7c1ceabd6a5a3175ce67b917
joegr <jpg88@georgetown.edu>
2019-01-06 12:42:25 -0600

    implement search

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
-
+import operator
 from django.views.generic import ListView, DetailView
 from blog.models import Post
 
 class PostList(ListView):
     model = Post
 
+    def get_queryset(self):
+
+        q = self.request.GET.get('q')
+        qs = Post.objects.all()
+
+        if q:
+            ql = q.split()
+            for i in ql:
+                qs = qs.filter(title__contains=i) | qs.filter(text__contains=i)
+
+        return qs
 
 class PostDetail(DetailView):
     model = Post
